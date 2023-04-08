072d496ef4d71d037332b64f8492319de2bb624e
russtack <stirlingest@gmail.com>
2023-04-08 11:17:45 -0400

    fina

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string secretWord = "USA";
+        string guess = "";
+        int guessCount = 0;
+        int guessLimit = 3;
+
+
+        Console.WriteLine("Guess the word!");
+        while (guessCount < guessLimit)
+        {
+            if (guess != secretWord)
+            {
+
+
+
+                guess = Console.ReadLine();
+
+                guessCount++;
+                if (guessCount < guessLimit && guess != secretWord)
+                {
+                    Console.WriteLine("Try again: ");
+
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("You win");
+                break;
+            }
+
+        }
+
+        if (guess != secretWord) { 
+        Console.WriteLine("You lost!"); }
+    }
+
+    
+}
